8a1875ffb4dfa70002452c5a7a969fa8421ce210
ruucm <ruucm.a@gmail.com>
2023-02-16 17:59:03 +0800

    ui/sub & exports config!

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Button, Card } from "ui";
+import { Button2 } from "ui/sub";
 
 const CARD_CONTENT = [
   {
@@ -27,21 +28,8 @@ export default function Home() {
       </Head>
 
       <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
-        <h1 className="mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl">
-          Web
-          <span className="block bg-gradient-to-r from-brandred to-brandblue bg-clip-text text-transparent px-2">
-            Turborepo Example
-          </span>
-        </h1>
-        <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
-          <Button />
-        </div>
-
-        <div className="mt-12 grid grid-cols-1 sm:grid-cols-3 gap-4 place-content-evenly">
-          {CARD_CONTENT.map((card) => (
-            <Card key={card.title} {...card} />
-          ))}
-        </div>
+        <Button />
+        <Button2 />
       </main>
     </div>
   );
